95f254665fa5cf2c5c1665d588142a93c3f54234
Wang Yichao <wangyichao@r-42-178-28-172.comp.nus.edu.sg>
2014-09-19 19:41:22 +0800

    add app.js

--- quizit/app.js
+++ quizit/app.js
@@ -1,37 +1,61 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
+var mongoskin = require('mongoskin');
+var db = mongoskin.db('mongodb://localhost:27017/quizit?auto_reconnect', {safe:true});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var quiz = require('./routes/quiz');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  req.db = {};
+  req.db.userbase = db.collection('userbase');
+  req.db.questionbase = db.collection('questionbase');
+  next();
+})
+
 // view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+    app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/quiz',quiz.list);
+app.post('/contribute',quiz.contributeQuestion);
 
-/// catch 404 and forward to error handler
+app.get('/render', function(req,res,next){
+    req.db.userbase.find({}).toArray(function(error,userbase ){
+    if (error) return next(error);
+    res.send(
+        userbase
+    );
+  });
+});
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
+    console.log("catch error");
     next(err);
 });
-
-/// error handlers
+// error handlers
 
 // development error handler
 // will print stacktrace
@@ -53,7 +77,11 @@ app.use(function(err, req, res, next) {
         message: err.message,
         error: {}
     });
+
 });
 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 module.exports = app;
