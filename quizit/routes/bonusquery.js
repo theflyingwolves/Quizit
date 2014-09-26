4bcdddb7172fac5bea50030f7931e0aeca587bd1
Wang Yichao <wangyichao@r-238-177-28-172.comp.nus.edu.sg>
2014-09-27 01:38:40 +0800

    final commit from backend

--- quizit/routes/bonusquery.js
+++ quizit/routes/bonusquery.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var bonusbase = require('../server/bonusbase');
 
-router.post('/',bonusbase.addbonus);
+router.post('/question',bonusbase.addbonusQuestion);
+router.post('/response',bonusbase.updatebonus);
+router.get('/',bonusbase.getbonusForUser);
 router.get('/target',bonusbase.getbonusForTarget);
 router.get('/player', bonusbase.getbonusForPlayer);
 
